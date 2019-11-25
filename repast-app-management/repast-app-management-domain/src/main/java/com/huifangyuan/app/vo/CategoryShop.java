677d4e2b558b1d2b44e56e423770920647c65445
yanzengjian <57744994+yanzengjian@users.noreply.github.com>
2019-11-25 21:30:41 +0800

    VO

---
+++ repast-app-management/repast-app-management-domain/src/main/java/com/huifangyuan/app/vo/CategoryShop.java
@@ -0,0 +1,14 @@
+package com.huifangyuan.app.vo;
+
+import lombok.Data;
+
+import java.io.Serializable;
+
+/**
+ * @author Administrator
+ */
+@Data
+public class CategoryShop implements Serializable {
+    private Long id;
+    private String name;
+}
