d3d214ae1f93aea9a58258c5e64fdcdf9d3fdbee
yanzengjian <57744994+yanzengjian@users.noreply.github.com>
2019-11-26 10:02:18 +0800

    根据关键字查询商品列表

--- repast-app-consumer-management/repast-app-product-consumer6081/src/main/java/com/huifangyuan/app/controller/ShopInfoController.java
+++ repast-app-consumer-management/repast-app-product-consumer6081/src/main/java/com/huifangyuan/app/controller/ShopInfoController.java
@@ -2,8 +2,10 @@ package com.huifangyuan.app.controller;
 
 import com.huifangyuan.app.base.BaseController;
 import com.huifangyuan.app.base.ResultData;
+import com.huifangyuan.app.domain.Product;
 import com.huifangyuan.app.domain.ProductCat;
 import com.huifangyuan.app.service.IRepastService;
+import com.huifangyuan.app.vo.ProductVo;
 import com.huifangyuan.app.vo.ShopInfoVo;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
@@ -38,6 +40,34 @@ public class ShopInfoController extends BaseController {
             System.out.println(repastService.getShopInfoByPrimaryKey(ShopId).getName());
             dataMap.put("Shop",repastService.getShopInfoByPrimaryKey(ShopId));
             return success(dataMap);
+        }
+        return failed();
+    }
+
+    /**
+     * 通过店铺 关键字搜素商品列表
+     * @param productVo
+     * @return
+     */
+    @PostMapping("/selectShopProduct")
+    @ApiOperation(value = "商品信息", notes = "通过店铺搜素查询商品列表")
+    public ResultData selectShopProduct(ProductVo productVo){
+        List<Product> products = repastService.selectShopProduct(productVo);
+        if (null != products){
+            return success("搜素成功",products);
+        }else {
+            return failed();
+        }
+    }
+    /**
+     * 进入积分商城，查询所有积分商品列表
+     */
+    @GetMapping("/selectIntegralProduct")
+    @ApiOperation(value = "积分商品列表", notes = "进入积分商城，查询所有积分商品列表")
+    public ResultData selectIntegralProduct(){
+        List<Product> products = repastService.selectIntegralProduct();
+        if (null != products){
+            return success("查询成功",products);
         }
         return failed();
     }
---
+++ repast-app-management/repast-app-management-mapper/src/main/java/com/huifangyuan/app/mapper/ProductMapper.java
@@ -0,0 +1,13 @@
+package com.huifangyuan.app.mapper;
+import com.huifangyuan.app.domain.Product;
+import com.huifangyuan.app.vo.ProductVo;
+import tk.mybatis.mapper.common.Mapper;
+
+import java.util.List;
+
+public interface ProductMapper extends Mapper<Product> {
+
+    List<Product> getProductByShopId(Long shopId);
+    List<Product> selectShopProduct(ProductVo productVo);
+    List<Product> getIntegralProduct();
+}
