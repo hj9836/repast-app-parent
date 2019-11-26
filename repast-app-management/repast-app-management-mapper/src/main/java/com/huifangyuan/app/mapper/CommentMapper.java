b82c23bcc3c4eb65914ffcb57340d810d6562945
Wu <56961638+965142000@users.noreply.github.com>
2019-11-26 17:07:25 +0800

    补：提交commentMapper

---
+++ repast-app-management/repast-app-management-mapper/src/main/java/com/huifangyuan/app/mapper/CommentMapper.java
@@ -0,0 +1,7 @@
+package com.huifangyuan.app.mapper;
+
+import com.huifangyuan.app.domain.Comment;
+import tk.mybatis.mapper.common.Mapper;
+
+public interface CommentMapper extends Mapper<Comment> {
+}
