b16c409dce261fb7cac86ba18942b6b7119e1bd7
eric0307 <1210580733@qq.com>
2019-11-26 14:25:47 +0800

    商品信息Controller

--- repast-app-consumer-management/repast-app-product-consumer6081/src/main/java/com/huifangyuan/app/controller/ProductInfoController.java
+++ repast-app-consumer-management/repast-app-product-consumer6081/src/main/java/com/huifangyuan/app/controller/ProductInfoController.java
@@ -2,7 +2,10 @@ package com.huifangyuan.app.controller;
 
 import com.huifangyuan.app.base.BaseController;
 import com.huifangyuan.app.base.ResultData;
+import com.huifangyuan.app.domain.Product;
+import com.huifangyuan.app.domain.ProductCat;
 import com.huifangyuan.app.service.IRepastService;
+import com.huifangyuan.app.vo.MemberProduct;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,6 +14,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.HashMap;
+import java.util.List;
 
 /**
  * @Company AAA软件教育
@@ -25,6 +29,60 @@ public class ProductInfoController extends BaseController {
 
     @Autowired
     private IRepastService repastService;
+
+    /**
+     *@ClassName ProductController
+     *@Description
+     *@Date 19:31 2019/11/21
+     * 根据店铺id获取所有商品类目信息
+     *@author eric
+     *@Param
+     *@Return
+     **/
+    @GetMapping("/getCatByShopId")
+    @ApiOperation(value = "商品类目",notes = "根据店铺id获取所有的商品类目信息")
+    public ResultData getCategoryByShopId(Long shopId){
+        List<ProductCat> catList = repastService.getCategoryByShopId(shopId);
+        if (null != catList){
+            return success(catList);
+        }
+        return failed();
+    }
+    /**
+     *@ClassName ProductController
+     *@Description
+     *@Date 22:28 2019/11/22
+     *@author eric
+     * 根据商品类目id 获取所有商品信息
+     *@Param
+     *@Return
+     **/
+    @GetMapping("/getProductByCategoryId")
+    @ApiOperation(value = "商品信息",notes = "根据商品类目信息获取所有商品信息列表")
+    public ResultData getProductByCategoryId(Long productCategoryId){
+        List<Product> productList = repastService.getProductByProductCategoryId(productCategoryId);
+        if (null != productList){
+            return success(productList);
+        }
+        return failed();
+    }
+    /*@ClassName ProductService
+     *@Description
+     *@Date 16:47 2019/11/23
+     *@author eric
+     *      根据商铺id获取所有商品信息 并判断促销类型 展示对应促销类型价格
+     *@Param
+     *@Return
+     **/
+    @GetMapping("/selectAllMemberProduct")
+    @ApiOperation(value = "商品信息",notes = "根据商铺id获取所有商品信息 并判断促销类型 展示对应促销类型价格")
+    public ResultData selectAllProduct(@RequestParam("shopId") Long shopId){
+        List<MemberProduct> memberProductList = repastService.selectAllProduct(shopId);
+        if (memberProductList.size()>0){
+            return success(memberProductList);
+        }
+        return failed();
+    }
 
 
 
