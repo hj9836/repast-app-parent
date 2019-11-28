1a6cf55f56fba7e3259178bd741d53b6a31d8725
Wu <56961638+965142000@users.noreply.github.com>
2019-11-28 21:22:30 +0800

    修改首页搜索的接口

--- repast-app-management/repast-app-management-service/src/main/java/com/huifangyuan/app/service/SearchService.java
+++ repast-app-management/repast-app-management-service/src/main/java/com/huifangyuan/app/service/SearchService.java
@@ -1,13 +1,9 @@
 package com.huifangyuan.app.service;
 
-import com.huifangyuan.app.base.BaseService;
 import com.huifangyuan.app.domain.Product;
-import com.huifangyuan.app.mapper.ProductInfoMapper;
-import com.huifangyuan.app.mapper.ProductMapper;
 import com.huifangyuan.app.mapper.SearchMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import tk.mybatis.mapper.common.Mapper;
 
 import java.util.List;
 
@@ -23,6 +19,13 @@ public class SearchService {
      * @return
      */
     public List<Product> HomeSeekProduct(String keywords){
+
+        if(null!=keywords){
+            keywords=("%"+keywords+"%");
+        } else {
+            keywords=("%%");
+        }
+
         return searchMapper.HomeSeekProduct(keywords);
     }
 
