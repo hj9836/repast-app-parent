3d9f1715b722a67f48d11d6e9888ec8654b20978
Wu <56961638+965142000@users.noreply.github.com>
2019-12-03 16:25:03 +0800

    吴文博产品评价店铺评价接口优化

--- repast-app-management/repast-app-management-domain/src/main/java/com/huifangyuan/app/vo/CommentVo.java
+++ repast-app-management/repast-app-management-domain/src/main/java/com/huifangyuan/app/vo/CommentVo.java
@@ -15,23 +15,17 @@ import java.util.Date;
 @Accessors(chain = true)
 public class CommentVo implements Serializable {
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    @Column(name = "shop_id")
     private Long shopId;
     /**
      * 订单ID
      */
-    @Column(name = "order_id")
     private Long orderId;
     /**
      * 订单为单一商品时，该字段有值
      */
-    @Column(name = "product_id")
     private Long productId;
-    @Column(name = "member_nick_name")
     private String memberNickName;
-    @Column(name = "product_name")
     private String productName;
     /**
      * 评价星数：0->5
@@ -40,20 +34,14 @@ public class CommentVo implements Serializable {
     /**
      * 评价的ip
      */
-    @Column(name = "member_ip")
     private String memberIp;
-    @Column(name = "create_time")
     private Date createTime;
-    @Column(name = "show_status")
     private Integer showStatus;
     /**
      * 购买时的商品属性
      */
-    @Column(name = "product_attribute")
     private String productAttribute;
-    @Column(name = "collect_couont")
     private Integer collectCouont;
-    @Column(name = "read_count")
     private Integer readCount;
     /**
      * 上传图片地址，以逗号隔开
@@ -62,9 +50,7 @@ public class CommentVo implements Serializable {
     /**
      * 评论用户头像
      */
-    @Column(name = "member_icon")
     private String memberIcon;
-    @Column(name = "replay_count")
     private Integer replayCount;
     private String content;
 
